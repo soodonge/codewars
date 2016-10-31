810ead70795ff24de36c244aecad6507feb36609
soodong <soodonge@gmail.com>
2016-10-31 15:30:17 +0900

    Opposite_number

---
+++ Opposite_number.js
@@ -0,0 +1,9 @@
+function opposite(number) {
+  if(number){
+    return -number
+  }else if(number === 0){
+    return 0;
+  }
+}
+
+module.exports = opposite;
---
+++ Opposite_number.test.js
@@ -0,0 +1,9 @@
+var chai = require('chai');
+var assert = chai.assert;
+var Opposite_number = require('./Opposite_number.js');
+
+describe('Opposite_number', function(){
+  it('should return opposite number',function(){
+      assert.equal(opposite(1), -1,)
+  })
+})
