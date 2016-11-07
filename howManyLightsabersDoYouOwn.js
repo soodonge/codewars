0045dbb2757d6f27373cbf3329c66895f8e84d3a
soodong <soodonge@gmail.com>
2016-11-07 18:51:47 +0900

    howManyLightsabersDoYouOwn

---
+++ howManyLightsabersDoYouOwn.js
@@ -0,0 +1,10 @@
+// TODO: Make some stuff more good here
+function howManyLightsabersDoYouOwn(nameOfPro){
+  if(nameOfPro === 'Zach'){
+    return 18;
+  }else{
+    return 0;
+  }
+}
+
+module.exports = howManyLightsabersDoYouOwn;
---
+++ howManyLightsabersDoYouOwn.test.js
@@ -0,0 +1,9 @@
+var chai = require('chai');
+var assert = chai.assert;
+var howManyLightsabersDoYouOwn = require('./howManyLightsabersDoYouOwn');
+
+describe("Solution", function(){
+  it("should test for something", function(){
+    assert.equal("actual", "expected", "This is just an example of how you can write your own TDD tests");
+  });
+});
